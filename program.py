831c3b2235e0953c8f6aea7dcceeb0d80d67ecf0
shalawatulshafa <shalawatulshafa@gmail.com>
2023-04-17 20:55:38 +0700

    program

--- program.py
+++ program.py
@@ -161,6 +161,29 @@ def hapusjin():
             else:
                 print("Tidak ada jin dengan username tersebut.")
                 break
+# F07 Jin pengumpul
+from random import randint
+def jumlahbahan () :
+    pasir = randint(0,5) 
+    batu = randint(0,5)
+    air = randint(0,5)
+    jumlahterkumpul = (f'Jin menemukan {pasir} pasir. {batu} batu, {air}')
+    return jumlahterkumpul
+print (jumlahbahan())
+
+# F12 Ayam berkokok
+def ayamberkokok() :
+    print ('Kukuruyuk.. Kukuruyuk..')
+    if jumlahcandi == 100 :
+        print (f'Jumlah Candi: {jumlahcandi}')
+        print ('Yah, Bandung Bondowoso memenangkan permainan!')
+    else :
+        print (f'Jumlah Candi : {jumlahcandi}')
+        print ('''Selamat, Roro Jonggrang memenangkan permainan!.
+
+Bandung Bondowoso angry noise.
+Roro Jonggrang dikutuk menjadi candi.''')       
+(ayamberkokok())
 
 def save():
     savefolder = input("Masukkan nama folder: ")
@@ -177,3 +200,67 @@ def save():
         parse.fromMatrix(data.bahan, data.elemBahan, os.path.join(savefolder, "bahan_bangunan.csv"))
         parse.fromMatrix(data.candi, data.elemCandi, os.path.join(savefolder, "candi.csv"))
         print(f"Berhasil menyimpan data di folder {savefolder}!")
+
+# F15 HELP
+def help() :
+    if  data.roleIn == 'bandung_bondowoso' :
+        print ('=========== HELP ===========')
+        print (''' 1. logout
+    Untuk keluar dari akun yang digunakan sekarang''')
+        print (''' 2. summonjin
+    Untuk memanggil jin''')
+        print (''' 3. Hilangkan Jin
+    Untuk menghapus jin dan candi yang telah dibangun oleh jin yang di hapus''')
+        print (''' 4. Ubah Tipe Jin
+    Untuk mengubah tipe jin pengumpul menjadi jin pembangun atau sebaliknya''')
+        print (''' 5. Batch Kumpul/Bangun
+    Untuk mengerahkan semua jin sesuai tipenya untuk melakukan tugasnya masing-masing''')
+        print (''' 6. Ambil Laporan Jin
+    Untuk mengambil laporan jin untuk mengetahui kinerja dari para jin''')
+        print (''' 7. Ambil Laporan Candi
+    Untuk mengambil laporan candi untuk mengetahui progress pembangunan candi ''')
+        
+    elif data.roleIn == 'roro_jonggrang' :
+        print ('=========== HELP ===========')
+        print (''' 1. logout
+    Untuk keluar dari akun yang digunakan sekarang''')
+        print (''' 2. Hancurkan Candi
+    Untuk menghancurkan candi agar menggagalkan rencana Bandung Bondowoso dan data candi terhapus''')
+        print (''' 3. Ayam Berkokok
+    Untuk menyelesaikan permainan dengan memalsukan pagi hari''')
+        print
+    elif data.roleIn == 'jin_pembangun' :
+        print ('=========== HELP ===========')
+        print (''' 1. logout
+    Untuk keluar dari akun yang digunakan sekarang''')
+        print (''' 2. Jin Pembangun
+    Untuk membangun candi dengan jumlah bahan yang tersedia jika jumlahnya mencukupi''')
+    elif data.roleIn == 'jin_pengumpul' :
+        print ('=========== HELP ===========')
+        print (''' 1. logout
+    Untuk keluar dari akun yang digunakan sekarang''')
+        print (''' 2. Jin Pengumpul
+    Untuk mengumpulkan bahan untuk membangun candi berupa pasir,batu dan air''')
+    elif data.roleIn == ' ' :
+        print ('=========== HELP ===========')
+        print ('''1. login
+   Untuk masuk menggunakan akun''')
+        print ('''2. exit
+   Untuk keluar dari program dan kembali ke terminal''')
+(help())
+
+#F16 EXIT
+def exit () :
+    pilihan = ("Apakah Anda mau melakukan penyimpanan file yang sudah diubah? ")
+    if pilihan == "n" or pilihan == "N" :
+        run = False
+    elif pilihan == "Y" or pilihan == "y" :
+        save()
+        run = False
+    else :
+        while pilihan != "n" or pilihan != "N" or pilihan != "y" or pilihan != "Y" :
+            input ("Apakah Anda mau melakukan penyimpanan file yang sudah diubah? ")
+
+
+
+
